e9aca007288d7c1fb47cf805fb70778b3b732872
ninolombardi <giannino.lombardi@mail.mcgill.ca>
2024-01-27 16:08:56 -0500

    Fix tile issues

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ import math
 import random 
 
 num_users = int(input("Please enter the number of players: "))
+game_level = int(input("Please select a level of difficulty (1/2/3): "))
+
+game = tile.Game(num_users, game_level)
+game.add_players()
+game.create_board()
+game.add_ladders()
+game.add_snakes()
+
 
 
--- tile.py
+++ tile.py
@@ -17,7 +17,6 @@ class Tile:
         self.is_laddertop = is_laddertop
         self.is_ladderbottom = is_ladderbottom
         
-START_TILE = Tile(1,None, False, False, False, False)
 OPERATORS_G1 = ["+", "-"]
 OPERATORS_G2 = ["+", "-", "x"] 
 OPERATORS_G3 = ["+", "-", "x", "/"] 
@@ -76,11 +75,11 @@ class Game:
     def add_players(self):
 
         i = 0
+        player_number = 1
         while (i < self.num_players):
-            player_number = 1
-            name_str = "Player " + player_number
-            new_player = Player(START_TILE, name_str)
-            self.players[i] = new_player
+            name_str = "Player " + str(player_number)
+            new_player = Player(Tile(1,None, False, False, False, False), name_str)
+            self.players.append(new_player)
             i += 1
             player_number += 1
 
@@ -104,7 +103,7 @@ class Game:
 
     
     def create_board(self):
-        self.tiles[0] = START_TILE
+        self.tiles.append(Tile(1,None, False, False, False, False))
         i = 1
         num_tile = 2
         
@@ -117,95 +116,95 @@ class Game:
                 op = OPERATORS_G3[random.randint(0, 3)]
 
             if (i == 2):
-                self.tiles[i] = Tile(num_tile, op, False, True, False, False)
+                self.tiles.append(Tile(num_tile, op, False, True, False, False))
                 num_tile += 1 
                 
             elif (i == 5):
-                self.tiles[i] = Tile(num_tile, op, False, True, False, False)
+                self.tiles.append(Tile(num_tile, op, False, True, False, False))
                 num_tile += 1 
 
             elif (i == 6):
-                self.tiles[i] = Tile(num_tile, op, False, False, True, False)
+                self.tiles.append(Tile(num_tile, op, False, False, True, False))
                 num_tile += 1 
                 
             elif (i == 11):
-                self.tiles[i] = Tile(num_tile, op, False, False, True, False)
+                self.tiles.append(Tile(num_tile, op, False, False, True, False))
                 num_tile += 1 
 
             elif (i == 12):
-                self.tiles[i] = Tile(num_tile, op, False, True, False, False)
+                self.tiles.append(Tile(num_tile, op, False, True, False, False))
                 num_tile += 1 
                 
             elif (i == 14):
-                self.tiles[i] = Tile(num_tile, op, False, False, False, True)
+                self.tiles.append(Tile(num_tile, op, False, False, False, True))
                 num_tile += 1
 
             elif (i == 15):
-                self.tiles[i] = Tile(num_tile, op, False, False, True, False)
+                self.tiles.append(Tile(num_tile, op, False, False, True, False))
                 num_tile += 1 
 
             elif (i == 16):
-                self.tiles[i] = Tile(num_tile, op, True, False, False, False)
+                self.tiles.append(Tile(num_tile, op, True, False, False, False))
                 num_tile += 1 
             
             elif (i == 18):
-                self.tiles[i] = Tile(num_tile, op, False, False, False, True)
+                self.tiles.append(Tile(num_tile, op, False, False, False, True))
                 num_tile += 1
             
             elif (i == 20):
-                self.tiles[i] = Tile(num_tile, op, True, True, False, False)
+                self.tiles.append(Tile(num_tile, op, True, True, False, False))
                 num_tile += 1
             
             elif (i == 21):
-                self.tiles[i] = Tile(num_tile, op, False, False, True, False)
+                self.tiles.append(Tile(num_tile, op, False, False, True, False))
                 num_tile += 1
             
             elif (i == 22):
-                self.tiles[i] = Tile(num_tile, op, False, True, False, True)
+                self.tiles.append(Tile(num_tile, op, False, True, False, True))
                 num_tile += 1
             
             elif (i == 23):
-                self.tiles[i] = Tile(num_tile, op, False, False, True, False)
+                self.tiles.append(Tile(num_tile, op, False, False, True, False))
                 num_tile += 1
             
             elif (i == 25):
-                self.tiles[i] = Tile(op, True, False, False, False)
+                self.tiles.append(Tile(num_tile, op, True, False, False, False))
                 num_tile += 1
             
             elif (i == 28):
-                self.tiles[i] = Tile(num_tile, op, False, False, False, True)
+                self.tiles.append(Tile(num_tile, op, False, False, False, True))
                 num_tile += 1
 
             elif (i == 31):
-                self.tiles[i] = Tile(num_tile, op, True, False, False, False)
+                self.tiles.append(Tile(num_tile, op, True, False, False, False))
                 num_tile += 1
             
             elif (i == 34):
-                self.tiles[i] = Tile(num_tile, op, True, False, False, False)
+                self.tiles.append(Tile(num_tile, op, True, False, False, False))
                 num_tile += 1
             
             elif (i == 35):
-                self.tiles[i] = Tile(num_tile, op, False, False, False, True)
+                self.tiles.append(Tile(num_tile, op, False, False, False, True))
                 num_tile += 1
 
             else: 
-                self.tiles[i] = Tile(num_tile, op, False, False, False, False)
+                self.tiles.append(Tile(num_tile, op, False, False, False, False))
                 num_tile += 1   
 
             i += 1
 
     def add_snakes(self):
-        self.snakes[0] = Snake(self.tiles[15], self.tiles[1])
-        self.snakes[1] = Snake(self.tiles[19], self.tiles[4])
-        self.snakes[2] = Snake(self.tiles[30], self.tiles[19])
-        self.snakes[3] = Snake(self.tiles[33], self.tiles[21])
+        self.snakes.append(Snake(self.tiles[15], self.tiles[1]))
+        self.snakes.append(Snake(self.tiles[19], self.tiles[4]))
+        self.snakes.append(Snake(self.tiles[30], self.tiles[19]))
+        self.snakes.append(Snake(self.tiles[33], self.tiles[21]))
     
     def add_ladders(self): 
-        self.ladders[0] = Ladder(self.tiles[17], self.tiles[5])
-        self.ladders[1] = Ladder(self.tiles[13], self.tiles[10])
-        self.ladders[2] = Ladder(self.tiles[21], self.tiles[14])
-        self.ladders[3] = Ladder(self.tiles[27], self.tiles[20])
-        self.ladders[4] = Ladder(self.tiles[34], self.tiles[22])
+        self.ladders.append(Ladder(self.tiles[17], self.tiles[5]))
+        self.ladders.append(Ladder(self.tiles[13], self.tiles[10]))
+        self.ladders.append(Ladder(self.tiles[21], self.tiles[14]))
+        self.ladders.append(Ladder(self.tiles[27], self.tiles[20]))
+        self.ladders.append(Ladder(self.tiles[34], self.tiles[22]))
     
 
 
