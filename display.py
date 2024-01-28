3a9338a1c051d5bad39a8994ef5323ce1d6f06be
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-27 19:20:53 -0500

    Add board display

--- display.py
+++ display.py
@@ -22,14 +22,20 @@ class Tile(pygame.sprite.Sprite):
         Initializes a Tile object 
 
         """
-        num_tiles_in_row = math.sqrt(NUM_TILES)
+        num_tiles_in_row = math.sqrt(NUM_TILES) # 6
         size = int(SCREEN_HEIGHT/num_tiles_in_row)
         super().__init__()
-        og_image = pygame.image.load('images/tile.png')
+        og_image = pygame.image.load('images/tile.png').convert_alpha()
         self.image = pygame.transform.scale(og_image, (size, size))
 
-        x_pos = ((num % num_tiles_in_row) - 1) * size + 2
-        if x_pos < 0: x_pos = size * (num_tiles_in_row - 1) + 2
+        # tile position
+        if (num < 7) or (num > 12 and num < 19) or (num > 24 and num < 31): 
+            x_pos = ((num % num_tiles_in_row) - 1) * size + 2
+            if x_pos < 0: x_pos = size * (num_tiles_in_row - 1) + 2
+        else: 
+            x_pos = (6 - (num % num_tiles_in_row)) * size + 2
+            if x_pos == 6 * size + 2: x_pos = 2
+
         y_pos = (7 - math.ceil(num/num_tiles_in_row)) * size + 2
         self.rect = self.image.get_rect(bottomleft = (x_pos, y_pos))
 
@@ -39,19 +45,44 @@ class Tile(pygame.sprite.Sprite):
         self.is_snaketail = is_snaketail
         self.is_laddertop = is_laddertop
         self.is_ladderbottom = is_ladderbottom
-         
 
-    
+    def draw_num(self):
+        # draw the number
+        font = pygame.font.Font(None, 28)
+        text_surface = font.render(str(self.num), True, (0, 0, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.topright = (self.rect.right - 15, self.rect.top + 15)
+        screen.blit(text_surface, text_rect)
+
+        # draw the operator
+        op_image = None;
+        if self.operator == 'plus': 
+            op_image = pygame.image.load('images/ops/add.png').convert_alpha()
+        elif self.operator == 'minus': 
+            op_image = pygame.image.load('images/ops/minus.png').convert_alpha()
+        elif self.operator == 'times':
+            op_image = pygame.image.load('images/ops/times.png').convert_alpha()
+        elif self.operator == 'divide': 
+            op_image = pygame.image.load('images/ops/divide.png').convert_alpha()
+
+        if op_image != None: 
+            image = pygame.transform.scale(op_image, (16, 16))
+            image_rect = image.get_rect()
+            image_rect.bottomright = (self.rect.right - 15, self.rect.bottom - 15)
+
+            screen.blit(image, image_rect)
+        
+
 
 
 # GROUPS
-# tile1 = Tile(1, None, False, False, False, False)
+# tile1 = Tile(1, 'plus', False, False, False, False)
 # tile = pygame.sprite.GroupSingle()
 # tile.add(tile1)
         
 all_tiles = pygame.sprite.Group()
 for i in range(1, NUM_TILES + 1):
-     tile = Tile(i, None, False, False, False, False)
+     tile = Tile(i, 'divide', False, False, False, False)
      all_tiles.add(tile)
         
 
@@ -65,8 +96,14 @@ while True:
 
     
     if game_active:
-         screen.fill((255,255,255))
-         all_tiles.draw(screen)
+        # all_tiles.update()
+        screen.fill((255,255,255))
+        # all_tiles.draw(screen)
+        all_tiles.draw(screen)
+        for tile in all_tiles.sprites():
+            tile.draw_num()
+
+
     else: None
 
     
