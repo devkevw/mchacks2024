71a0820ec776ba606189ca0c5aa080755473850f
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-27 20:45:49 -0500

    Add snakes and ladders

--- display.py
+++ display.py
@@ -8,6 +8,9 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
 game_active = True
 NUM_TILES = 36
+OPERATORS = ['+', '-', 'x', '/']
+level = 1;
+rolldie = False
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -22,21 +25,21 @@ class Tile(pygame.sprite.Sprite):
         Initializes a Tile object 
 
         """
-        num_tiles_in_row = math.sqrt(NUM_TILES) # 6
-        size = int(SCREEN_HEIGHT/num_tiles_in_row)
+        tiles_in_row = math.sqrt(NUM_TILES) # 6
+        size = int(SCREEN_HEIGHT/tiles_in_row)
         super().__init__()
         og_image = pygame.image.load('images/tile.png').convert_alpha()
         self.image = pygame.transform.scale(og_image, (size, size))
 
         # tile position
         if (num < 7) or (num > 12 and num < 19) or (num > 24 and num < 31): 
-            x_pos = ((num % num_tiles_in_row) - 1) * size + 2
-            if x_pos < 0: x_pos = size * (num_tiles_in_row - 1) + 2
+            x_pos = ((num % tiles_in_row) - 1) * size + 2
+            if x_pos < 0: x_pos = size * (tiles_in_row - 1) + 2
         else: 
-            x_pos = (6 - (num % num_tiles_in_row)) * size + 2
+            x_pos = (6 - (num % tiles_in_row)) * size + 2
             if x_pos == 6 * size + 2: x_pos = 2
 
-        y_pos = (7 - math.ceil(num/num_tiles_in_row)) * size + 2
+        y_pos = (7 - math.ceil(num/tiles_in_row)) * size + 2
         self.rect = self.image.get_rect(bottomleft = (x_pos, y_pos))
 
         self.num = num
@@ -56,13 +59,13 @@ class Tile(pygame.sprite.Sprite):
 
         # draw the operator
         op_image = None;
-        if self.operator == 'plus': 
+        if self.operator == '+': 
             op_image = pygame.image.load('images/ops/add.png').convert_alpha()
-        elif self.operator == 'minus': 
+        elif self.operator == '-': 
             op_image = pygame.image.load('images/ops/minus.png').convert_alpha()
-        elif self.operator == 'times':
+        elif self.operator == 'x':
             op_image = pygame.image.load('images/ops/times.png').convert_alpha()
-        elif self.operator == 'divide': 
+        elif self.operator == '/': 
             op_image = pygame.image.load('images/ops/divide.png').convert_alpha()
 
         if op_image != None: 
@@ -74,25 +77,153 @@ class Tile(pygame.sprite.Sprite):
         
 
 
+class sideDisplay(pygame.sprite.Sprite): 
+    def __init__(self):
+        super().__init__()
 
-# GROUPS
-# tile1 = Tile(1, 'plus', False, False, False, False)
-# tile = pygame.sprite.GroupSingle()
-# tile.add(tile1)
+    def draw_text(self):
+        # draw the number
+        font = pygame.font.Font(None, 28)
+        text_surface = font.render('Turn', True, (0, 0, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        screen.blit(text_surface, text_rect)
+
         
+
+
+# GROUPS and FUNCTIONS
+        
+side_display = pygame.sprite.GroupSingle()
+side_display.add(sideDisplay())
+       
 all_tiles = pygame.sprite.Group()
-for i in range(1, NUM_TILES + 1):
-     tile = Tile(i, 'divide', False, False, False, False)
-     all_tiles.add(tile)
-        
+# generate tiles
+def generate_Tiles(level): 
+    for i in range (1, 37):
+        # levels
+        if (level == 1):
+            op = OPERATORS[random.randint(0, 1)]
+        elif (level == 2): 
+            op = OPERATORS[random.randint(0, 2)]
+        else: # level 3
+            op = OPERATORS[random.randint(0, 3)]
+
+        # tiles
+        if i == 1: 
+            tile = Tile(i, None, False, False, False, False)
+        else: 
+            if (i == 2):
+                tile = (Tile(i, op, False, True, False, False))
+                
+            elif (i == 5):
+                tile = (Tile(i, op, False, True, False, False))
+                 
+            elif (i == 6):
+                tile = (Tile(i, op, False, False, True, False))  
+                
+            elif (i == 11):
+                tile = (Tile(i, op, False, False, True, False))
+                 
+            elif (i == 12):
+                tile = (Tile(i, op, False, True, False, False))
+                 
+            elif (i == 14):
+                tile = (Tile(i, op, False, False, False, True))
+                
+            elif (i == 15):
+                tile = (Tile(i, op, False, False, True, False))
+
+            elif (i == 16):
+                tile = (Tile(i, op, True, False, False, False))
+                 
+            elif (i == 18):
+                tile = (Tile(i, op, False, False, False, True))
+                
+            elif (i == 20):
+                tile = (Tile(i, op, True, True, False, False))
+                
+            elif (i == 21):
+                tile = (Tile(i, op, False, False, True, False))
+                
+            elif (i == 22):
+                tile = (Tile(i, op, False, True, False, True))
+                
+            elif (i == 23):
+                tile = (Tile(i, op, False, False, True, False))
+                
+            elif (i == 25):
+                tile = (Tile(i, op, True, False, False, False))
+                
+            elif (i == 28):
+                tile = (Tile(i, op, False, False, False, True))
+                
+            elif (i == 31):
+                tile = (Tile(i, op, True, False, False, False))
+                
+            elif (i == 34):
+                tile = (Tile(i, op, True, False, False, False))
+                
+            elif (i == 35):
+                tile = (Tile(i, op, False, False, False, True))
+                
+            else: 
+                tile = (Tile(i, op, False, False, False, False))
+                   
+        all_tiles.add(tile)
+
+generate_Tiles(level)
+
+def draw_ladders(all_tiles): 
+     tiles_list = [
+         [6, 18],
+         [11, 14],
+         [15, 22],
+         [21, 28],
+         [23, 35]
+     ]
+
+     for tiles in tiles_list: 
+        tile1 = all_tiles.sprites()[tiles[0]-1]
+        tile2 = all_tiles.sprites()[tiles[1]-1]
+        pygame.draw.line(screen, (139, 69, 19), tile1.rect.center, tile2.rect.center, 5)
+    
+def draw_snakes(all_tiles): 
+    tiles_list = [
+         [2, 16],
+         [5, 20],
+         [20, 31],
+         [22, 34],
+         [12, 25]
+    ]
+
+    dot_size = 3
+    dot_spacing = 10
+
+    for tiles in tiles_list:
+        tile1 = all_tiles.sprites()[tiles[0] - 1]
+        tile2 = all_tiles.sprites()[tiles[1] - 1]
+
+        # Calculate the direction vector and normalized distance
+        direction = pygame.math.Vector2(tile2.rect.center[0] - tile1.rect.center[0],
+                                         tile2.rect.center[1] - tile1.rect.center[1])
+        normalized_direction = direction.normalize()
+
+        # Draw dotted line
+        dot_position = pygame.math.Vector2(tile1.rect.center)
+        while dot_position.distance_to(tile2.rect.center) >= dot_spacing:
+            pygame.draw.circle(screen, (34, 139, 34), (int(dot_position.x), int(dot_position.y)), dot_size)
+            dot_position += normalized_direction * dot_spacing
+    
 
 
+    
 # display game itself
 while True:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
+                sys.exit()
 
     
     if game_active:
@@ -103,10 +234,16 @@ while True:
         for tile in all_tiles.sprites():
             tile.draw_num()
 
+        side_display.sprites()[0].draw_text()
+
+        # draw ladders and snakes
+        draw_ladders(all_tiles)
+        draw_snakes(all_tiles)
+            
+
 
     else: None
 
-    
 
     pygame.display.update()
     clock.tick(60)
