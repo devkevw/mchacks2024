1b6dff0f039f82a88e04ddb059c79c2141a353b5
zuhayr <zuhayrmahmood@yahoo.ca>
2024-01-28 12:51:18 -0500

    fixed imports on certain files

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import functions
-import tile 
+import game
 
 
 num_users = int(input("Please enter the number of players: "))
 game_level = int(input("Please select a level of difficulty (1/2/3): "))
 
 
-game = tile.Game(num_users, game_level)
+game = game.Game(num_users, game_level)
 game.add_players()
 game.create_board()
 game.add_ladders()
